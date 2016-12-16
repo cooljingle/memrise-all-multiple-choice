c224ad6e72587aadf44e41812a3519a7f1ffb6e5
Aaron Miles <cool_jingle@hotmail.co.uk>
2016-12-16 22:09:16 +0000

    $(document).ready required

--- Memrise_All_Multiple_Choice.user.js
+++ Memrise_All_Multiple_Choice.user.js
@@ -1,32 +1,34 @@
 // ==UserScript==
-// @name           Memrise All Multiple Choice 
+// @name           Memrise All Multiple Choice
 // @namespace      https://github.com/cooljingle
 // @description    All multiple choice when doing Memrise typing courses
 // @match          http://www.memrise.com/course/*/garden/*
 // @match          http://www.memrise.com/garden/review/*
-// @version        0.0.1
+// @version        0.0.2
 // @updateURL      https://github.com/cooljingle/memrise-all-multiple-choice/raw/master/Memrise_All_Multiple_Choice.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-all-multiple-choice/raw/master/Memrise_All_Multiple_Choice.user.js
 // @grant          none
 // ==/UserScript==
 
-MEMRISE.garden.boxes.load = (function() {
-    var cached_function = MEMRISE.garden.boxes.load;
-    return function() {
-        enableAllMultipleChoice();
-        return cached_function.apply(this, arguments);
-    };
-}());
-
-function enableAllMultipleChoice() {
-    MEMRISE.garden.session.box_factory.make = (function() {
-        var cached_function = MEMRISE.garden.session.box_factory.make;
+$(document).ready(function() {
+    MEMRISE.garden.boxes.load = (function() {
+        var cached_function = MEMRISE.garden.boxes.load;
         return function() {
-            var result = cached_function.apply(this, arguments);
-            if(result.template === "typing") {
-                result.template = "multiple_choice";
-            }
-            return result;
+            enableAllMultipleChoice();
+            return cached_function.apply(this, arguments);
         };
     }());
-}
+
+    function enableAllMultipleChoice() {
+        MEMRISE.garden.session.box_factory.make = (function() {
+            var cached_function = MEMRISE.garden.session.box_factory.make;
+            return function() {
+                var result = cached_function.apply(this, arguments);
+                if(result.template === "typing") {
+                    result.template = "multiple_choice";
+                }
+                return result;
+            };
+        }());
+    }
+});
