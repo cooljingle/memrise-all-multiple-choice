5ab5f718de1525ff0402d2e523e4894b6a3bba9a
Aaron Miles <cool_jingle@hotmail.co.uk>
2017-12-12 12:37:38 +0000

    improved multiple choice enforcement

--- Memrise_All_Multiple_Choice.user.js
+++ Memrise_All_Multiple_Choice.user.js
@@ -4,7 +4,7 @@
 // @description    All multiple choice when doing Memrise typing courses
 // @match          https://www.memrise.com/course/*/garden/*
 // @match          https://www.memrise.com/garden/review/*
-// @version        0.0.3
+// @version        0.0.4
 // @updateURL      https://github.com/cooljingle/memrise-all-multiple-choice/raw/master/Memrise_All_Multiple_Choice.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-all-multiple-choice/raw/master/Memrise_All_Multiple_Choice.user.js
 // @grant          none
@@ -24,9 +24,10 @@ $(document).ready(function() {
             var cached_function = MEMRISE.garden.session.box_factory.make;
             return function() {
                 var result = cached_function.apply(this, arguments);
-                if(result.template === "typing") {
+                var shouldMultipleChoice = MEMRISE.garden.box_mapping[result.template].prototype.isTestBox &&
+                    MEMRISE.garden.session.box_factory.isTestPossible(result, "multiple_choice");
+                if(shouldMultipleChoice)
                     result.template = "multiple_choice";
-                }
                 return result;
             };
         }());
